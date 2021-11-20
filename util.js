c76d0d7b72a65c607a58de10935fa2ae7bef8352
kvssankar <kvs.sankar001@gmail.comm>
2021-11-20 09:20:52 +0530

    case ins

--- util.js
+++ util.js
@@ -1,12 +1,12 @@
 const fullSearch = (searchtext) => {
   var arr = searchtext.split(" ");
   var qarr = [];
-  var qstr = "select keyword from keywords where keyword like $1";
+  var qstr = "select keyword from keywords where lower(keyword) like lower($1)";
   for (var i = 0; i < arr.length; i++) {
     qarr.push("%" + arr[i] + "%");
   }
   for (var i = 2; i <= arr.length; i++) {
-    qstr += " and keyword like $" + i;
+    qstr += " and lower(keyword) like lower($" + i + ")";
   }
   return { qstr, qarr };
 };
@@ -14,12 +14,12 @@ const fullSearch = (searchtext) => {
 const partialSearch = (searchtext) => {
   var arr = searchtext.split(" ");
   var qarr = [];
-  var qstr = "select keyword from keywords where keyword like $1";
+  var qstr = "select keyword from keywords where lower(keyword) like lower($1)";
   for (var i = 0; i < arr.length; i++) {
     qarr.push("%" + arr[i] + "%");
   }
   for (var i = 2; i <= arr.length; i++) {
-    qstr += " or keyword like $" + i;
+    qstr += " or lower(keyword) like lower($" + i + ")";
   }
   qstr += " except " + fullSearch(searchtext).qstr;
   return { qstr, qarr };
